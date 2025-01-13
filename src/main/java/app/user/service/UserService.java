9f36a0b8d98c6cf8233f1d38a91c5850874607aa
Viktor Aleksandrov <viktorostrov07@gmail.com>
2025-01-14 00:51:51 +0200

    Remove unused imports

--- src/main/java/app/user/service/UserService.java
+++ src/main/java/app/user/service/UserService.java
@@ -17,10 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
 
 @Slf4j
 @Service
