430d2dd6a7c72281798cc5e923a70d9181b64afa
Viktor Aleksandrov <viktorostrov07@gmail.com>
2025-01-13 23:30:28 +0200

    Initial Commit

--- src/main/java/app/subscription/service/SubscriptionService.java
+++ src/main/java/app/subscription/service/SubscriptionService.java
@@ -9,8 +9,6 @@ import app.user.model.User;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
